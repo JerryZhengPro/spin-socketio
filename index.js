322177994a62ab6052a31672359f8d0162d4894b
Jerry Zheng <65697699+JerryZhengPro@users.noreply.github.com>
2022-08-04 22:38:41 +0000

    Set up for production

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://spin.jerrytq.com'),
     methods: ['GET', 'POST']
   }
 });
+
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const users = [];
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 app.post('/item-bought', (request, response) => {
-    for (const user of users) {
-      user.emit('item bought', Number(request.body.marketID), request.body.seller, Number(request.body.price));
-    }
-    response.send();
+  for (const user of users) {
+    user.emit('item bought', Number(request.body.marketID), request.body.seller, Number(request.body.price));
+  }
+  response.send();
 });
 
-app.post('/item-listed', (request, response) => { 
-    for (const user of users) {
-      user.emit('item listed', request.body);
-    }
-    response.send();
+app.post('/item-listed', (request, response) => {
+  for (const user of users) {
+    user.emit('item listed', request.body);
+  }
+  response.send();
 });
 
 app.post('/item-unboxed', (request, response) => {
-    for (const user of users) {
-      user.emit('item unboxed', request.body.itemName, request.body.rarity, request.body.unboxer);
-    }
-    response.send();
+  for (const user of users) {
+    user.emit('item unboxed', request.body.itemName, request.body.rarity, request.body.unboxer);
+  }
+  response.send();
 });
 
 io.on('connection', (socket) => {
@@ -43,11 +43,11 @@ io.on('connection', (socket) => {
     console.log('A user has disconnected from the server!');
     for (let i = 0; i < users.length; i++) {
       if (users[i].id === socket.id) {
-        users.splice(i, 1); 
+        users.splice(i, 1);
       }
     }
 
   });
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
