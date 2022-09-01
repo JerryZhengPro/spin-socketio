c85bb7c80759dcfb59addf31ced4a90c4a7c6827
JerryZhengPro <jerryzhengpro@gmail.com>
2022-09-01 13:02:03 -0700

    Add a home route

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const io = require('socket.io')(server, {
 });
 
 const authenticate = (req, res, next) => {
-  if (req.method !== 'POST') {
-    res.status(400);
-    res.send();
-    return;
-  }
-  else if (req.header('Authorization') !== process.env.SOCKET_KEY) {
+  if (req.header('Authorization') !== process.env.SOCKET_KEY) {
     res.status(401);
     res.send();
     return;
@@ -22,7 +17,6 @@ const authenticate = (req, res, next) => {
     next();
   }
 }
-app.use(authenticate);
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,17 +24,21 @@ app.use(bodyParser.json());
 
 const users = new Map();
 
-app.post('/item-bought', (request, response) => {
+app.get('/', (_, response) => {
+  response.send('Server is online!');
+});
+
+app.post('/item-bought', authenticate, (request, response) => {
   io.emit('item bought', Number(request.body.marketID), request.body.seller, Number(request.body.price));
   response.send();
 });
 
-app.post('/item-listed', (request, response) => {
+app.post('/item-listed', authenticate, (request, response) => {
   io.emit('item listed', request.body);
   response.send();
 });
 
-app.post('/item-unboxed', (request, response) => {
+app.post('/item-unboxed', authenticate, (request, response) => {
   io.emit('item unboxed', request.body.itemName, request.body.rarity, request.body.unboxer);
   response.send();
 });
