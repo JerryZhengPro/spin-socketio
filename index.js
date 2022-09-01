78dea43ec2ee0885002be80c47fe58eaba81f67b
JerryZhengPro <jerryzhengpro@gmail.com>
2022-09-01 11:55:40 -0700

    Improve tracking of clients

--- index.js
+++ index.js
@@ -28,41 +28,30 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const users = [];
+const users = new Map();
 
 app.post('/item-bought', (request, response) => {
-  for (const user of users) {
-    user.emit('item bought', Number(request.body.marketID), request.body.seller, Number(request.body.price));
-  }
+  io.emit('item bought', Number(request.body.marketID), request.body.seller, Number(request.body.price));
   response.send();
 });
 
 app.post('/item-listed', (request, response) => {
-  for (const user of users) {
-    user.emit('item listed', request.body);
-  }
+  io.emit('item listed', request.body);
   response.send();
 });
 
 app.post('/item-unboxed', (request, response) => {
-  for (const user of users) {
-    user.emit('item unboxed', request.body.itemName, request.body.rarity, request.body.unboxer);
-  }
+  io.emit('item unboxed', request.body.itemName, request.body.rarity, request.body.unboxer);
   response.send();
 });
 
 io.on('connection', (socket) => {
-  users.push(socket);
+  users.set(socket.id, null);
   console.log('A user has connected to the server!');
 
   socket.on('disconnect', () => {
     console.log('A user has disconnected from the server!');
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].id === socket.id) {
-        users.splice(i, 1);
-      }
-    }
-
+    users.delete(socket.id);
   });
 });
 
